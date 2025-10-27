1196032a1aba5a0479b177f02f8be53143821a13
SofiaKubo <skuboreva@gmail.com>
2025-10-27 15:20:03 +0100

    feat(validation): implement input and options checks in analyzeSalesData

--- src/main.js
+++ src/main.js
@@ -35,7 +35,33 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
   // @TODO: Проверка входных данных
+  if (
+    !data ||
+    !Array.isArray(data.sellers) ||
+    !Array.isArray(data.products) ||
+    !Array.isArray(data.purchase_records) ||
+    data.sellers.length === 0 ||
+    data.products.length === 0 ||
+    data.purchase_records.length === 0
+  ) {
+    throw new Error("Некорректные входные данные");
+  }
   // @TODO: Проверка наличия опций
+  if (!options || typeof options !== "object") {
+    throw new Error("Опции не переданы");
+  }
+
+  const { calculateRevenue, calculateBonus } = options;
+
+  if (
+    !calculateRevenue ||
+    !calculateBonus ||
+    typeof calculateRevenue !== "function" ||
+    typeof calculateBonus !== "function"
+  ) {
+    throw new Error("Некорректные опции: отсутствуют функции расчета");
+  }
+
   // @TODO: Подготовка промежуточных данных для сбора статистики
   // @TODO: Индексация продавцов и товаров для быстрого доступа
   // @TODO: Расчет выручки и прибыли для каждого продавца
