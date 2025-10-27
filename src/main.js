ce2cd47860c6f11d7e82b5caf713b35d864e90b9
SofiaKubo <skuboreva@gmail.com>
2025-10-27 16:53:12 +0100

    feat(indexing): create seller and product indexes for fast data access

--- src/main.js
+++ src/main.js
@@ -73,6 +73,13 @@ function analyzeSalesData(data, options) {
   }));
 
   // @TODO: Индексация продавцов и товаров для быстрого доступа
+  const sellerIndex = Object.fromEntries(
+    sellerStats.map((seller) => [seller.id, seller])
+  );
+
+  const productIndex = Object.fromEntries(
+    data.products.map((product) => [product.sku, product])
+  );
 
   // @TODO: Расчет выручки и прибыли для каждого продавца
   // @TODO: Сортировка продавцов по прибыли
